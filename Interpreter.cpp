7fbbdc4071ea222bf4fc4a662c8e60910908714f
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2024-07-09 00:39:58 +0300

    Addded separate function to validate input before parsing it; + new functionalities

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -14,12 +14,13 @@ public:
         return find(begin(functions), end(functions), token) != end(functions);
     }
     
-private: 
     bool isOperator(char symbol)
     {
         return (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '=');
     }
-    
+    bool assignment = false;
+private:
+
         bool validateParentheses(vector<string>& tokens)
         {
             stack<string> temp;
@@ -99,18 +100,27 @@ private:
         }
 
 public:     
-    bool assignment = false;
+    
+
+
     vector<string> ParseInput(string& input)
     {
         bool function = false;
         vector<string> tokens;
         int i = 0;
         int countAssignment = 0;
+
+        if (input.substr(0, 3) == "var")
+        {
+            assignment = true;
+            input.erase(0, 3);
+           
+        }
+
         while (i < input.length())
         {
             if (isdigit(input[i]) || (input[i] == '-' && (i == 0 || !isdigit(input[i - 1]))))
             {
-
                 string current_token = "";
                 if (input[i] == '-')
                 {
@@ -138,64 +148,42 @@ public:
                         break;
                     }
                 }
-                if (!isFunction) {
-                    cout << "Invalid input!" << endl;
-                    return {};
+                if (!isFunction)
+                {
+                    string variable = "";
+                    while (i < input.length() && (isalpha(input[i]) || isdigit(input[i])))
+                    {
+                        variable += input[i];
+                        i++;
+                    }
+                    tokens.push_back(variable);
                 }
             }
-            else {
-                if (isOperator(input[i]))
-                {
-                    if (i >= 0 && !isdigit(input[i - 1]) && input[i - 1] != ')' && input[i] != '-')
-                    {
-                        cout << "Invalid input!" << endl;
-                        return {};
-                    }
-                    if (input[i] == '=')
-                    {
-                        assignment = true;
-                        countAssignment++;
-                        if (countAssignment > 1 || input[0] == '=' || (i>0 && isOperator(input[i-1])) )
-                        {
-                            cout << "Invalid input !" << endl;
-                            return {};
-                        }
-                    }
-                    tokens.push_back(string(1, input[i]));
-                }
-                else if (input[i] == ')' || input[i] == '(' || input[i] == ',')
-                {
-                    if ((input[i] == '(' && input[i + 1] == ')') || (input[i] == '(' && input[i + 2] == ')' && input[i + 1] == ','))
-                    {
-                        cout << "Invalid input!" << endl;
-                        return {};
-                    }
-                    tokens.push_back(string(1, input[i]));
-                }
-                else {
-                    cout << "Invalid input !" << endl;
-                    return {};
-                }
+            else
+            {
+                tokens.push_back(string(1, input[i]));
                 i++;
             }
-        } 
-        
+        }
+
         if (!validateParentheses(tokens))
         {
             cout << "Parentheses mismatch!" << endl;
             return {};
         }
-        
+
         if (function)
         {
-if (!validateFunctions(tokens))
-        {
-            cout << "Functions mismatch!" << endl;
-            return {};
+            if (!validateFunctions(tokens))
+            {
+                cout << "Functions mismatch!" << endl;
+                return {};
+            }
         }
-        }
+
         return tokens;
     }
+
 };
 
 class ReversePolishNotation
@@ -348,16 +336,115 @@ class ResultOnScreen
 {
     ReversePolishNotation notation;
     Tokens tokens;
+    bool assignment = false;
+
 public:
+
+    bool validateInput(string& input)
+    {
+        int countAssignment = 0;
+        int commaCount = 0;
+        int functionCount = 0;
+        assignment = false;
+        if (input.empty())
+        {
+            return false;
+        }
+        for (int i = 0; i < input.length(); i++)
+        {
+            if (tokens.isOperator(input[i]))
+            {
+                if (i == 0 || (!isdigit(input[i - 1]) && input[i - 1] != ')' && input[i] != '-' && input[i] != '='))
+                {
+                    return false;
+                }
+            }
+                else if (input[i] == '=')
+                {
+                    assignment = true;
+                    if (input.substr(0, 3) != "var")
+                    {
+                        return false;
+                    }
+                    countAssignment++;
+                    if (countAssignment > 1 || i == 0 || tokens.isOperator(input[i - 1]))
+                    {
+                        return false;
+                    }
+                }
+            
+            
+                else if (input[i] == ',')
+                {
+                    commaCount++;
+                }
+                else if ((input[i] == '(' && input[i + 1] == ')') || (input[i] == '(' && input[i + 2] == ')' && input[i + 1] == ','))
+                {
+                    return false;
+                }
+            
+            else if (isalpha(input[i]))
+            {
+                bool isFunction = false;
+                for (int j = 0; j < 4; j++)
+                {
+                    if (input.substr(i, tokens.functions[j].length()) == tokens.functions[j])
+                    {
+                        i += tokens.functions[j].length();
+                        isFunction = true;
+                        functionCount++;
+                    }
+                }
+                if (!isFunction) {
+                    if (assignment)
+                    {                       
+                        int start = 3;
+                        while (i < input.length() && isalpha(input[i]) && input[i] != '=') {
+                            i++;
+                        }                      
+                    }
+                    else
+                    {
+                        continue;
+                    }                    
+             }
+            else if (isdigit(input[i]) || (i > 0 && i < input.length() && input[i] == '.' && isdigit(input[i - 1]) && isdigit(input[i + 1])))
+            {
+                continue;
+            }
+            else {
+              return false;
+            }
+            }
+            if (commaCount != functionCount)
+            {
+                return false;
+            }           
+            return true;
+        }
+    }
     void TakeUserInput(string& input)
-    {        
+    {
         input.erase(remove(input.begin(), input.end(), ' '), input.end());
+
         vector<string> parts = tokens.ParseInput(input);
-        if (!tokens.assignment)
+        if (parts.empty())
+        {
+            cout << "Parsing error!" << endl;
+            return;
+        }
+
+        if (tokens.assignment)
+        {           
+            string variable_name = parts[0];
+            parts.erase(parts.begin(), parts.begin() + 2); 
+            double var_value = notation.GetResult(parts);
+            cout << variable_name << " = " << var_value << endl;                     
+        }
+        else
         {
             cout << "Result: " << notation.GetResult(parts) << endl;
         }
-
     }
 
 };
@@ -368,15 +455,16 @@ int main()
     while (true)
     {
         string input;
+        Tokens tokens;
         cout << "Enter an expression:" << endl;
         getline(cin, input);
+        ResultOnScreen result;
 
-        while (input.empty()) {
-            cout << "Input cannot be empty. Enter an expression:" << endl;
+        while (!result.validateInput(input)) {
+            cout << "Invalid input! Enter an expression:" << endl;
             getline(cin, input);
         }
 
-        ResultOnScreen result;
         result.TakeUserInput(input);
     }
 
