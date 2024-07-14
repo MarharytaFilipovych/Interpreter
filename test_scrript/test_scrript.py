53ba8871e2fc36e6d8bd6b41793f04f28075e58c
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2024-07-14 12:26:20 +0300

    test

--- test_scrript/test_scrript.py
+++ test_scrript/test_scrript.py
@@ -15,12 +15,10 @@ def run_echo(input_data):
 def test_echo():
     tests = [
         {"input": (
-            "1 + 2\n"
             "var a = 10\n"
             "a + 4\n"
             "var omega = min(3,4)*6\n"
             "var c = max(300, 100 * max(2,3))*200\n"
-            "3*(8.3+9.2822992)-min(8,8.0000000000001)/2829\n"
             "max(a,c)\n"
             "var b = pow(a,3)\n"
             "c = b / 7.99898988\n"
@@ -32,12 +30,10 @@ def test_echo():
             "exit\n"
         ),
         "expected_output": (
-            ">Echo: 3\n"
             ">Echo: a = 10\n"
             ">Echo: 14\n"
             ">Echo: omega = 18\n"
             ">Echo: c = 60000\n"
-            ">Echo: 52.7441\n"
             ">Echo: 60000\n"
             ">Echo: b = 1000\n"
             ">Echo: c = 125.016\n"
@@ -54,7 +50,6 @@ def test_echo():
             "min(0,)\n"
             "min3\n"
             "exit\n"
-
         ),
         "expected_output": (
             ">Invalid input!\n"
@@ -62,7 +57,53 @@ def test_echo():
             ">Invalid input!\n"
             ">Invalid input!\n"
             ">Invalid input!\n>"
+        )},
+        {"input": (
+            "1 + 2\n"           
+            "3*(8.3+9.2822992)-min(8,8.0000000000001)/2829\n" 
+            "2345889900-(2*3+7/222222*(6789022222+2222222))\n"
+            "14+89\n"
+            "exit\n"
+        ),
+        "expected_output": (
+            ">Echo: 3\n"
+            ">Echo: 52.7441\n" 
+            ">Echo: 2.34567e+09\n"
+            ">Echo: 103\n>"
+        )},
+         {"input": (
+            "var a = 12/0\n"           
+            "var c = 12/a\n"   
+            "c+a\n"
+            "exit\n"
+        ),
+        "expected_output": (
+            ">Echo: a = inf\n"
+            ">Echo: c = 0\n"
+            ">Echo: inf\n>"
+        )},
+         {"input": (
+            "var comma = 45.897\n"           
+            "var delta = 4.00045\n"   
+            "var sab = -333.44432\n"
+            "abs(sab)\n"
+            "sab\n"
+            "pow(comma, delta)\n"
+            "pow(delta,comma)\n"
+            "pow(delta,0.00000000032)\n"
+            "exit\n"
+        ),
+        "expected_output": (
+            ">Echo: comma = 45.897\n"
+            ">Echo: delta = 4.00045\n"
+            ">Echo: sab = -333.444\n"
+            ">Echo: 333.444\n"
+            ">Echo: -333.444\n"
+            ">Echo: 4.44514e+06\n"
+            ">Echo: 4.31509e+27\n"
+            ">Echo: 1\n>"
         )}
+       
     ]
 
     for test in tests:
